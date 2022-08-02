428b7b44c4c0cae09b74f1a8320e092fe0fb2829
Dewayne VanHoozer <dvanhoozer@gmail.com>
2022-08-02 15:19:17 -0500

    replaced bottel block with one from just

--- Formula/justprep.rb
+++ Formula/justprep.rb
@@ -6,7 +6,12 @@ class Justprep < Formula
   license "MIT"
 
   bottle do
-    root_url "https://ghcr.io/v2/madbomber/justprep"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "df2cf86c253f460bfd8946b308e7b55fbf543e9b794b329c28bfda75258f35f8"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4101ad9fbeb30cf08e47c97ef125254fc95a6dbc6f67d331eae37d5adfe20438"
+    sha256 cellar: :any_skip_relocation, monterey:       "a9e7d7744d50642800d30fd71bd1186969afa62c7bbb1acab00449f2e7d8d51c"
+    sha256 cellar: :any_skip_relocation, big_sur:        "9f2cdddfff055157be9642a70d0d3feee46ce378019e50cd4d00bdc0b01e8fa7"
+    sha256 cellar: :any_skip_relocation, catalina:       "8db7da8d57c737df68d636c10fb92c4e9134057ff74e381f33cd3995fc0df2e7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3b964ce56de96ac15559c5fa7b6bf58df83fe5972bc1b54c1519b436b72a3018"
   end
 
   depends_on "crystal"  => :build
