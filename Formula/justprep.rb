f7c99d46b9bd0b048d6159254b41d990a21bfa99
Dewayne VanHoozer <dvanhoozer@gmail.com>
2022-08-03 11:34:03 -0500

    one last attempt at a bottle block this time guessing at parameters with real sha256 from asset fukes ub tge real repo

--- Formula/justprep.rb
+++ Formula/justprep.rb
@@ -5,6 +5,23 @@ class Justprep < Formula
   sha256 "bc031467bf2b6be227b80f18331257eb143baa994a106c65fb46372ed740c06f"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "891e110314f0704e3dfdc03b53a6d2fd059ddb444d25834713ca0b9dad1bb489"  
+    # justprep-v1.2.4.14-aarch64-unknown-linux-gnu.tar.gz
+
+    # "80b97f8a98f1f98c7be55130d67f8401ac70787c1d27459b4a705ae9e7619892"  
+    # justprep-v1.2.4.14-x86_64-unknown-linux-musl.tar.gz
+
+    sha256 cellar: :any_skip_relocation, arm64_monterey:  "0dfe62d080b4a39574e2cb05ae4043860e5eb8890147b6f412fb66893d731bc7"  
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:   "0dfe62d080b4a39574e2cb05ae4043860e5eb8890147b6f412fb66893d731bc7"  
+    # justprep-v1.2.4.14-arm64-apple-darwin.tar.
+
+    sha256 cellar: :any_skip_relocation, monterey:    "b6bdb6eae7bc2bed021ba8e9e7fd3ffa7a8bc9317db535408b95c3380eaf18c6"  
+    sha256 cellar: :any_skip_relocation, big_sur:     "b6bdb6eae7bc2bed021ba8e9e7fd3ffa7a8bc9317db535408b95c3380eaf18c6"  
+    # justprep-v1.2.4.14-x86_64-apple-darwin.tar.gz
+  end
+
+
   depends_on "crystal"  => :build
   depends_on "just"     => :build
 
